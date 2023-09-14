7e3d7505f1730208535f40f015d5db3aa65cc499
Timpan42 <tim@fagerdal.se>
2023-09-14 11:13:34 +0200

    💄 style

--- src/componets/TodoButtons.jsx
+++ src/componets/TodoButtons.jsx
@@ -1,7 +1,7 @@
 import './TodoButtons.css'
 
 function TodoButtons(props) {
-    let {todos, setTodos} = props
+    let { todos, setTodos } = props
 
     const addTodo = () => {
         const newTodo = document.getElementById('newTodo').value
@@ -12,23 +12,14 @@ function TodoButtons(props) {
     }
 
     const checkAll = () => {
-        const allTrue = 0
         const newTodos = todos.map(todo => {
-                return todo.completed === false ? { ...todo, completed: !todo.completed } : todo
-        })
-        setTodos(newTodos)
-    }
-
-    const unCheckAll = () => {
-        const allTrue = 0
-        const newTodos = todos.map(todo => {
-                return todo.completed === true ? { ...todo, completed: !todo.completed } : todo
+            return todos[0].completed === false ? { ...todo, completed: true } : {...todo, completed: false}
         })
         setTodos(newTodos)
     }
 
     const deleteSelected = () => {
-        const newTodos = todos.filter (todo => todo.completed === true ? null : todo)
+        const newTodos = todos.filter(todo => todo.completed === true ? null : todo)
         setTodos(newTodos)
     }
 
@@ -36,17 +27,16 @@ function TodoButtons(props) {
         setTodos([])
         localStorage.setItem('index', 0)
     }
-    
+
 
     return (
         <>
             <div className='ButtonContainer'>
-                <input id="newTodo" type='text' />
-                <button onClick={() => { addTodo() }}>Write</button>
-                <button onClick={() => { checkAll() }}>Check all</button>
-                <button onClick={() => { unCheckAll() }}>Uncheck all</button>
-                <button onClick={() => { deleteSelected() }}>Delete checked</button>
-                <button onClick={() => { deleteAll() }}>Delete all</button>
+                <input id="newTodo" type='text'/>
+                    <button className ="button" onClick={() => { addTodo() }}>Write</button>
+                    <button className ="button" onClick={() => { checkAll() }}>Check/Uncheck all</button>
+                    <button className ="button" onClick={() => { deleteSelected() }}>Delete checked</button>
+                    <button className ="button" onClick={() => { deleteAll() }}>Delete all</button>
             </div>
         </>
     )
